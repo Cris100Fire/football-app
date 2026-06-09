de1e807c39455d0260ad6daeda53a50b1cd4d7f8
Cristopher <cienfirescris14@gmail.com>
2026-06-09 15:25:14 -0500

    Another commit

--- src/pages/TeamPage.jsx
+++ src/pages/TeamPage.jsx
@@ -1,4 +1,4 @@
-import { useParams, Link } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import { useTeamDetail } from "../hooks/useTeamDetail";
 import styles from "../styles/pages/Teampage.module.css";
 import TeamDetail from "../components/teams/TeamDetail";
